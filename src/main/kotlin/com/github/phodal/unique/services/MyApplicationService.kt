640f4a141cafe4a57246b85c7da881c0fe289d07
GitHub Action <action@github.com>
2021-11-01 11:18:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/phodal/unique/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/phodal/unique/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.phodal.unique.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phodal.unique.MyBundle
 
 class MyApplicationService {
 
